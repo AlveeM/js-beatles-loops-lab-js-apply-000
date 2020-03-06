db8092d9ff8c8e0e46e10573679eba507dc7d6ef
Alvee Noor Akand <alvee.akand@gmail.com>
2020-03-06 22:11:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(arrMusicians, arrInstruments) {
+  let result = [];
+  for (let i = 0; i < arrMusicians; i++) {
+    let string = `${arrMusicians[i]} plays ${arrInstruments[i]}`;
+    result.push(string);
+  }
+}
